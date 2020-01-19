228da7aad4ef5f30ae3a8f4756ce8604186dcdbd
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-19 20:49:15 -0300

    chore: websocket connection with socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-2qe63.mongodb.net/omnistack10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -18,7 +24,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+server.listen(3333, () => {
   console.log('Server running on port 3333');
 });
-
